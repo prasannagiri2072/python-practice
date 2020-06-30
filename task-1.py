9c917018b9874f46237eafbcb8fa9d66b0ecbe89
prasannagiri2072 <67471866+prasannagiri2072@users.noreply.github.com>
2020-06-30 18:36:26 +0545

    Update task-1.py

--- task-1.py
+++ task-1.py
@@ -1,4 +1,6 @@
 print("Twinkle,twinkle little star")
 print("\t how I wonder what you are!")
 print("\t\t Up above the world so high,")
+print("\t\tLike a diamond in the sky ")
+print("Twinkle,twinkle little star")
         
