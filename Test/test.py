5d6550d3f502b04ecd35b8d060e4f3e1f79ba19f
prasannagiri2072 <giriprasanna375@gmail.com>
2020-11-04 20:25:14 +0545

    assignment

---
+++ Test/test.py
@@ -0,0 +1,12 @@
+# "What will be the output of the following Python code?
+x = "abcdef"
+i = "a"
+while i in x:
+    x = x[:-1]
+    print(i, end = "" "")
+# a)        i i i i i i
+# b)        a a a a a a
+# c)        a a a a a
+# d)        none of the mentioned
+
+# "
